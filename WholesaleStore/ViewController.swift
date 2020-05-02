3873d6cae19cb740eb550c07b74238eceb21d2ba
Abdullah <abdullah.alsalamah18@gmail.com>
2020-05-03 01:50:51 +0300

    Initial Commit

---
+++ WholesaleStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WholesaleStore
+//
+//  Created by Abdullah Alsalamah on 03/05/2020.
+//  Copyright © 2020 Abdullah Alsalamah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
